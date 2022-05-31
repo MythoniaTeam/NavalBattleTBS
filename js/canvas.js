b6c6162a4e507ed481f456d1932d5833a462037c
Heaveeeen <1393908967@qq.com>
2022-05-31 23:34:59 +0800

    加入getFloatedGrid
    一个函数，用于判断光标所在的正六边形格子的坐标（其实为了这玩意我和允某曾经苦恼过好久，但没想到最终写出来就只有不到50行）（老天保佑千万别出bug）

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,4 @@
-ctx = document.getElementById("canvas").getContext("2d");
+var ctx = document.getElementById("canvas").getContext("2d");
 
 class Vector {
     //2维向量或坐标
@@ -96,7 +96,54 @@ class Camera {
         var y = s * (cameraSize.y / 2 - this.scale * (p.y - this.p.y));
         return new Vector(x, y);
     }
-
 }
 
-defaultCamera = new Camera();
+var defaultCamera = new Camera();
+
+function getFloatedGrid(cp, a) {
+    //根据光标位置算出光标所在的格子在棋盘中的坐标
+    //cp = cursor position
+    //a是正六边形边长的一半
+    var tp = new Vector((cp.x / Math.sqrt(3) * a + (cp.y / a - 2) / 3) / 2 - 1, (cp.y / a - 2) / 3);
+    var gp = new Vector(Math.round(tp.x), Math.round(tp.y));//grid position
+    //transformed position，把光标坐标转换到棋盘坐标系
+    var lines = {
+        k: {k: 0, b: gp.y},
+        l: {k: -1, b: gp.x + gp.y},
+        m: {k: 0.5, b: -0.5 * gp.x + g.y + 0.5},
+        n: {k: 2, b: -2 * gp.x + gp.y + 1},
+        p: {k: 0.5, b: -0.5 * gp.x + g.y - 0.5},
+        q: {k: 2, b: gp.x + gp.y - 1}
+    };
+    
+    function f(l) {
+        //算出点tp是否在一条直线（点斜式）的上方
+        return tp.y > l.k * tp.x + l.b
+    };
+    
+    var offset = new Vector();
+    
+    if (f(lines.k)) {
+        if (f(lines.l)) {
+            if (f(lines.m)) {
+                offset = new Vector(0, 1);
+            }
+        } else {
+            if (f(lines.n)) {
+                offset = new Vector(-1, 0);
+            }
+        }
+    } else {
+        if (f(lines.l)) {
+            if (!f(lines.q)) {
+                offset = new Vector(1, 0);
+            }
+        } else {
+            if (!f(lines.p)) {
+                offset = new Vector(0, -1);
+            }
+        }
+    };
+    
+    return gp.plus(offset);
+}
